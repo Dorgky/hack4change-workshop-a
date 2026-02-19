e8bf797a6ed94ff8a584d25521ce5c03b6f5681a
Sebastian Whalen <sebwhalen@gmail.com>
2026-02-19 19:37:17 -0400

    Slight improvements to code organization and extra documentation.

--- hack4change-server-csharp/API/Program.cs
+++ hack4change-server-csharp/API/Program.cs
@@ -1,23 +1,20 @@
-using Hack4Change.API;
+/// For this application, we're using a very simple structure. The API is defined entirely in this file, along with all the server settings.
+/// If your application is more complex then feel free to reorganize or replace this as needed.
+
+using Hack4Change.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Services are injected here so that they can be created and/or provided as necessary for any API requests.
+builder.Services.AddScoped<MessageProvider>();
 
+// After services are injected, we use the builder to create the web application.
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-app.MapGet("/hello", () =>
-    MessageProvider.WelcomeMessage)
+// All of the APIs are created here. For now there's only one, which provides a welcome message to the user.
+app.MapGet("/hello", (MessageProvider messageProvider) =>
+    messageProvider.WelcomeMessage())
 .WithName("GetHello");
 
+// Once all the configuration is completed, the app is launched.
 app.Run();
